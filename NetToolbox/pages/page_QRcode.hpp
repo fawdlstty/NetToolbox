d4f0505d369537ce5c5201abc758fd32fa2a9208
fawdlstty <f@fawdlstty.com>
2020-11-05 17:56:21 +0800

    prune solution

--- NetToolbox/pages/page_QRcode.hpp
+++ NetToolbox/pages/page_QRcode.hpp
@@ -7,14 +7,7 @@
 #include <tchar.h>
 
 #include "../tools/tool_Gdip.hpp"
-extern "C" {
-#include "../../QRCode/qrcode.h"
-}
-#ifdef _DEBUG
-#pragma comment (lib, "../lib/QRCode_d.lib")
-#else
-#pragma comment (lib, "../lib/QRCode.lib")
-#endif
+#include "../3rdparty/QrCode/QrCode.hpp"
 
 #include "page_base.hpp"
 
@@ -37,68 +30,41 @@ public:
 			faw::String _str_bkimg = m_qrcode_bkimg->GetText ();
 
 			// 生成数据
-			QRCode qrcode;
-			//// begin 测试
-			//for (size_t _i = 20; _i <= 20; ++_i) {
-			//	uint8_t *_data = new uint8_t[qrcode_getBufferSize (_i)];
-			//	qrcode_initText (&qrcode, _data, _i, ECC_MEDIUM, _str.c_str ());
-			//	faw::String _path = faw::String::format (_T ("D:/__img__test_/_%d_%d.png"), _i, qrcode.size);
-			//	if (m_bmp)
-			//		delete m_bmp;
-			//	m_bmp = new Gdiplus::Bitmap (qrcode.size * 5, qrcode.size * 5, PixelFormat32bppARGB);
-			//	Gdiplus::Graphics g (m_bmp);
-			//	Gdiplus::Color _bcolor = Gdiplus::Color::MakeARGB (255, 0, 0, 0);
-			//	Gdiplus::Color _wcolor = Gdiplus::Color::MakeARGB (255, 255, 255, 255);
-			//	Gdiplus::Brush *_bbrush = new Gdiplus::SolidBrush (_bcolor);
-			//	Gdiplus::Brush *_wbrush = new Gdiplus::SolidBrush (_wcolor);
-			//	for (size_t y = 0; y < (size_t) qrcode.size; ++y) {
-			//		for (size_t x = 0; x < (size_t) qrcode.size; ++x) {
-			//			bool _is_black = qrcode_getModule (&qrcode, (uint8_t) x, (uint8_t) y);
-			//			bool _identfer = _is_identfer (_i, x, y);
-			//			if (_identfer) {
-			//				g.FillRectangle (_is_black ? _bbrush : _wbrush, Gdiplus::Rect (x * 5, y * 5, 5, 5));
-			//			} else {
-			//				m_bmp->SetPixel (x * 5 + 2, y * 5 + 2, _is_black ? _bcolor : _wcolor);
-			//			}
-			//		}
-			//	}
-			//	tool_Gdip::gdip_save (m_bmp, _path.strw ().c_str ());
-			//	delete[] _data;
+			const qrcodegen::QrCode qr = qrcodegen::QrCode::encodeText (_str.c_str (), qrcodegen::QrCode::Ecc::LOW);
+			//QRCode qrcode;
+			//uint8_t _ver = 1, *_data = nullptr;
+			//for (; _ver <= 40; ++_ver) {
+			//	if (_data)
+			//		delete[] _data;
+			//	_data = new uint8_t[qrcode_getBufferSize (_ver)];
+			//	bool _succ = qrcode_initText (&qrcode, _data, _ver, ECC_MEDIUM, _str.c_str ()) >= 0;
+			//	if (_succ)
+			//		break;
 			//}
-			//// end 测试
-			uint8_t _ver = 1, *_data = nullptr;
-			for (; _ver <= 40; ++_ver) {
-				if (_data)
-					delete[] _data;
-				_data = new uint8_t[qrcode_getBufferSize (_ver)];
-				bool _succ = qrcode_initText (&qrcode, _data, _ver, ECC_MEDIUM, _str.c_str ()) >= 0;
-				if (_succ)
-					break;
-			}
-			if (_ver > 40) {
-				::MessageBox (m_parent->GetHWND (), International::translate (_T ("Content is too long, the generation of two-dimensional code failed")).data (), International::translate (_T ("Info")).data (), MB_ICONHAND);
-				delete[] _data;
-				return true;
-			}
+			//if (_ver > 40) {
+			//	::MessageBox (m_parent->GetHWND (), International::translate (_T ("Content is too long, the generation of two-dimensional code failed")).data (), International::translate (_T ("Info")).data (), MB_ICONHAND);
+			//	delete[] _data;
+			//	return true;
+			//}
 
 			// 生成图片
 			if (m_bmp)
 				delete m_bmp;
-			m_bmp = new Gdiplus::Bitmap (qrcode.size * 5, qrcode.size * 5, PixelFormat32bppARGB);
+			m_bmp = new Gdiplus::Bitmap (qr.getSize () * 5, qr.getSize () * 5, PixelFormat32bppARGB);
 			Gdiplus::Graphics g (m_bmp);
 			Gdiplus::Brush *_bbrush = new Gdiplus::SolidBrush (Gdiplus::Color::MakeARGB (255, 0, 0, 0));
 			Gdiplus::Brush *_wbrush = new Gdiplus::SolidBrush (Gdiplus::Color::MakeARGB (255, 255, 255, 255));
-			for (size_t y = 0; y < (size_t) qrcode.size; ++y) {
-				for (size_t x = 0; x < (size_t) qrcode.size; ++x) {
-					g.FillRectangle (qrcode_getModule (&qrcode, (uint8_t) x, (uint8_t) y) ? _bbrush : _wbrush, Gdiplus::Rect (x * 5, y * 5, 5, 5));
-					_is_identfer (_ver, x, y);
+			for (size_t y = 0; y < (size_t) qr.getSize (); ++y) {
+				for (size_t x = 0; x < (size_t) qr.getSize (); ++x) {
+					g.FillRectangle (qr.getModule (x, y) ? _bbrush : _wbrush, Gdiplus::Rect (x * 5, y * 5, 5, 5));
+					_is_identfer (qr.getVersion (), x, y);
 				}
 			}
 			//tool_Gdip::gdip_save (m_bmp, L"d:/text.png");
 			delete _wbrush;
 			delete _bbrush;
 
-			delete[] _data;
+			//delete[] _data;
 			return true;
 		} else if (name == _T ("qrcode_save")) {
 
